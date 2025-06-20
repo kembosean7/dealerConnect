399e7dc1e9b525b738420035f6475febbd9d86a7
Sean Kembo <sekembojhb024@student.wethinkcode.co.za>
2025-06-20 05:00:11 +0200

    implement filter chain on auth endpoint

--- dealerConnect/src/main/java/com/example/dealerConnect/config/JwtAuthenticationFilter.java
+++ dealerConnect/src/main/java/com/example/dealerConnect/config/JwtAuthenticationFilter.java
@@ -5,11 +5,18 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -17,6 +24,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain)
 
             throws ServletException, IOException {
+
+        String path = request.getServletPath();
+        if(path.startsWith("api/v1/auth")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+
+
+
+
 
     }
 }
---
+++ dealerConnect/src/main/java/com/example/dealerConnect/config/JwtService.java
@@ -0,0 +1,4 @@
+package com.example.dealerConnect.config;
+
+public class JwtService {
+}
