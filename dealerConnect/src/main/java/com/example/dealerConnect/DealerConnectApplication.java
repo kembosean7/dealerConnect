3be4891a333b5300dd87153259293308b35d0014
Sean Kembo <sekembojhb024@student.wethinkcode.co.za>
2025-06-20 03:49:51 +0200

    adding project structure files

---
+++ dealerConnect/src/main/java/com/example/dealerConnect/DealerConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dealerConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerConnectApplication.class, args);
+	}
+
+}
