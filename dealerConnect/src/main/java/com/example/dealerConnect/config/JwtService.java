f26dd578b6c05ca8555f5fcde170179cb4b3a29c
Sean Kembo <sekembojhb024@student.wethinkcode.co.za>
2025-06-23 22:43:22 +0200

    Implement method that extracts username from jwt token

--- dealerConnect/src/main/java/com/example/dealerConnect/config/JwtService.java
+++ dealerConnect/src/main/java/com/example/dealerConnect/config/JwtService.java
@@ -15,6 +15,10 @@ public class JwtService {
 
     private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+
+    }
 
     private <T> T extractClaim(String token, Function<Claims, T> claimsTFunction) {
         final Claims claims = extractAllClaims(token);
